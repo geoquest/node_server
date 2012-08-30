826b334e8759c7b1a48521b29e20b7ff5c48a747
guest-wels <guest-wels@POOL-C-00.bit.uni-bonn.de>
2012-08-30 13:47:15 +0200

    first upload

--- ServerComponents/server.js
+++ ServerComponents/server.js
@@ -7,6 +7,7 @@ everyauth.debug = true;
 var usersById = {};
 var nextUserId = 0;
 
+
 function addUser (source, sourceUser) {
   var user;
   if (arguments.length === 1) { // password-based
@@ -61,10 +62,13 @@ var app = express.createServer(
   , everyauth.middleware()
 );
 
+
 app.configure(function() {
   app.set('port', process.env.PORT || 3000)
   app.set('views', 'views')
-  app.set('view engine', 'jade')
+  app.engine('html', require('ejs').renderFile);
+  app.set("view options", {layout: false})
+ // app.set('view engine', 'jade')
   app.use(express.favicon())
   app.use(express.logger('dev'))
   app.use(express.cookieParser('scrt'))
@@ -72,11 +76,22 @@ app.configure(function() {
   app.use(everyauth.middleware())
   app.use(express.bodyParser())
   app.use(express.methodOverride())
-  app.use(express.static(__dirname + '/public'))
+  app.use(express.static(__dirname))
   app.use(preEveryauthMiddlewareHack());
   app.use(everyauth.middleware());
   app.use(postEveryauthMiddlewareHack());
 })
+
+
+
+//app.engine('.html', {
+//	  compile: function(str, options){
+//	    return function(locals) {
+//	 	    var compiled = _.template(str);
+//	        return compiled(locals);
+//	    };
+//	  }
+//	});
 
 function preEveryauthMiddlewareHack() {
     return function (req, res, next) {
@@ -112,7 +127,13 @@ function postEveryauthMiddlewareHack() {
 };
 
 app.get('/', function (req, res) {
-  res.render('home');
+	var supplies = new Array("name");
+	res.render('index.ejs', { title: 'Login' , supplies: supplies});
+});
+
+app.get('/login/geoquestUser', function (req, res) {
+	var supplies = new Array("name");
+	res.render('index.ejs', { title: 'Login' , supplies: supplies});
 });
 
 everyauth.helpExpress(app);
@@ -120,5 +141,6 @@ everyauth.helpExpress(app);
 app.listen(3000);
 
 console.log('Go to http://local.host:3000');
+console.log(__dirname);
 
 module.exports = app;
