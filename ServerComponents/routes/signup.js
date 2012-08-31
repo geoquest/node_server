d1d8a8fb2462daba7db952881d212f67f287fb2a
chiu <chiu@POOL-C-03.bit.uni-bonn.de>
2012-08-31 12:28:52 +0200

    Add password confirmation in SignUp.

--- ServerComponents/routes/signup.js
+++ ServerComponents/routes/signup.js
@@ -9,24 +9,31 @@ function signup(req, res){
 }
 
 function handleSignupPost(req, res){
-
-    userDB.insertGQUser(
-            req.param('username'),
-            req.param('password'),
-            req.param('fName'),
-            req.param('lName'),
-            req.param('email'),
-            function(err, result){
-                console.log(result);
-                if(result){
-                    console.log("SignUp for a new GQUser done");
-                    res.render('signupResult.ejs', { title: 'SignUp Succeed.', result: 'Hi, ' +  req.param('fName') + '!'});
-                } else{
-                    console.log("SignUp failed.");
-                    res.render('signupResult.ejs', { title: 'SignUp Failed.', result: 'Please retry.'});                    
+    if(req.param('password') == req.param('confirmPassword')){
+        userDB.insertGQUser(
+                req.param('username'),
+                req.param('password'),
+                req.param('fName'),
+                req.param('lName'),
+                req.param('email'),
+                function(err, result){
+                    console.log(result);
+                    if(result){
+                        console.log("SignUp for a new GQUser done");
+                        res.render('signupResult.ejs', { title: 'SignUp Succeed.', result: 'Hi, ' +  req.param('fName') + '!'});
+                    } else{
+                        console.log("SignUp failed.");
+                        res.render('signupResult.ejs', { title: 'SignUp Failed.', result: 'Please retry.'});                    
+                    }
                 }
-            }
-    );
+        );
+        console.log( "Data user:" + req.param('username') + "password" + req.param('password', null) + "email" + req.param('email', null));
+
+    }
+    else{
+        
+        res.render('signupResult.ejs', { title: 'Password not matched.', result: 'Please retry.'});
+        
+    }
     
-    console.log( "Data user:" + req.param('username') + "password" + req.param('password', null) + "email" + req.param('email', null));
 }
