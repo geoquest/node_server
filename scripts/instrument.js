15949fc801166416e8c8817a4ab430f12c258db6
Matthias Molitor <matthias@matthimatiker.de>
2012-08-31 12:24:20 +0200

    fixed paths according to project

--- scripts/instrument.js
+++ scripts/instrument.js
@@ -1,10 +1,10 @@
 // Save the original source files.
 var fs = require('fs');
-fs.renameSync(__dirname + "/../www", __dirname + "/../www-original");
+fs.renameSync(__dirname + "/../ServerComponents", __dirname + "/../ServerComponents-original");
 
 // Instrument the source files.
 var process = require('child_process');
-var command = "jscoverage --no-highlight " + __dirname + "/../www-original  " + __dirname + "/../www";
+var command = "jscoverage --no-highlight " + __dirname + "/../ServerComponents-original  " + __dirname + "/../ServerComponents";
 console.log("Executing: ");
 console.log(command);
 process.exec(command, function (error, stdout, stderr) {
--- scripts/restore.js
+++ scripts/restore.js
@@ -1,8 +1,8 @@
 // Remove instrumented files.
 // Use a 3rd party script, as there is no way to remove a directory with all files in node.
 var rimraf = require("rimraf");
-rimraf(__dirname + "/../www", function() {
+rimraf(__dirname + "/../ServerComponents", function() {
 	// Restore the original source files.
 	var fs = require('fs');
-	fs.renameSync(__dirname + "/../www-original", __dirname + "/../www");
+	fs.renameSync(__dirname + "/../ServerComponents-original", __dirname + "/../ServerComponents");
 });
