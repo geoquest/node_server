b5b78d706a5cfc214df731b587fb558c87e41cd8
Matthias Molitor <matthias@matthimatiker.de>
2012-09-03 17:30:09 +0200

    added module for basic User type documented how it should work

---
+++ ServerComponents/User.js
@@ -0,0 +1,14 @@
+/**
+ * This module provides a basic user class that holds
+ * all necessary data and provides some convenience methods.
+ * 
+ * Create a new User instance:
+ * <code>
+ * var type = require("User");
+ * var newUser = new type.class();
+ * </code>
+ */
+
+exports.class = function() {
+	
+}
