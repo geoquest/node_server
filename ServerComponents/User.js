520cf8119e199cc5b9bcb78eaa9b2d6640664fdd
Matthias Molitor <matthias@matthimatiker.de>
2012-09-04 12:41:38 +0200

    documented attributes

--- ServerComponents/User.js
+++ ServerComponents/User.js
@@ -11,10 +11,58 @@
 
 User = function() 
 {
-	this._username = null;
+	/**
+	 * Holds the type of the user.
+	 * 
+	 * Valid values are (case-sensitive):
+	 * # GeoQuest
+	 * # Google
+	 * # Facebook
+	 * 
+	 * @var {String}
+	 */
+	this.loginType = null;
+	
+	/**
+	 * Identifier for this user that is unique among this loginType.
+	 * 
+	 * Contains 1 of the following id types:
+	 * # chosen username for GeoQuest users
+	 * # number provided by Google
+	 * # number provided by Facebook
+	 * 
+	 * @var {String}
+	 */
+	this._identifier = null;
+	
+	/**
+	 * Encrypted password of the user.
+	 * 
+	 * The password is only available for GeoQuest users.
+	 * 
+	 * @var {String}|null
+	 */
 	this._password = null;
+	
+	/**
+	 * The firstname of the user.
+	 * 
+	 * @var {String}
+	 */
 	this._firstname = null;
+	
+	/**
+	 * The lastname of the user.
+	 * 
+	 * @var {String}
+	 */
 	this._lastname = null;
+	
+	/**
+	 * Email address if it was provided (only by GeoQuest users).
+	 * 
+	 * @var {String}|null
+	 */
 	this._email = null;
 	
 };
