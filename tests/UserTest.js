3e5d24fafd5e4ded833ccda633ab4cef2dbb1117
Matthias Molitor <matthias@matthimatiker.de>
2012-09-04 14:35:45 +0200

    added setup/teardown functions

--- tests/UserTest.js
+++ tests/UserTest.js
@@ -1,12 +1,25 @@
 var assert = require("assert");
 
-var type = require("../ServerComponents/User.js");
+var User = require("../ServerComponents/User.js");
 
 describe('User', function() {
+	var user = null;
+	
+	beforeEach(function() {
+		user =  new User.class();
+	});
+	
+	afterEach(function() {
+		user = null;
+	});
+	
     describe('constructor', function() {
         it('should create a User instance', function() {
-            var user = new type.class();
-            assert.ok(user instanceof type.class);
+            assert.ok(user instanceof User.class);
         });
+    });
+    
+    describe('set', function() {
+    	
     });
 });
