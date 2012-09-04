fd2bb06f6d0ab2c6607b0c42606d1e78f8967288
Matthias Molitor <matthias@matthimatiker.de>
2012-09-04 16:23:00 +0200

    added user repository stub documented how it should work

---
+++ ServerComponents/UserDataAccess.js
@@ -0,0 +1,10 @@
+/**
+ * This module provides basic methods to query for users.
+ * 
+ * Example:
+ * <code>
+ * var UserRepository = require("UserDataAccess");
+ * var repository = new UserRepository.class(mongoDBConnection);
+ * var user = repository.byGoogleIdentifier("1513235656");
+ * </code>
+ */
