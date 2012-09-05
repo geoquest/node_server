23d3653a788c184a5a225ace09085be07c2bf1a4
Matthias Molitor <matthias@matthimatiker.de>
2012-09-05 17:45:15 +0200

    implemented tests

--- tests/DependencyInjectorTest.js
+++ tests/DependencyInjectorTest.js
@@ -53,22 +53,58 @@ describe('DependencyInjector', function() {
 	
 	describe('inject', function() {
 		it('throws exception if no object is provided', function() {
-			
+			assert.throws(function() {
+				injector.inject('invalid content');
+			});
 		});
 		it('does nothing if no dependency can be injected', function() {
-			
+			assert.doesNotThrow(function() {
+				injector.inject({});
+			});
 		});
 		it('returns provided object', function() {
-			
+			var object = {
+				'id': 'object 42'
+			};
+			var result = injector.inject(object);
+			assert.strictEqual(result, object);
 		});
 		it('injects dependency if function is available', function() {
-			
+			var object = {
+			    name: null,
+				setName: function(name) {
+					// Stores the provided name in an object
+					// attribute if this function is called.
+					this.name = name;
+				}
+			};
+			injector.inject(object);
+			assert.equal(object.name, 'GeoQuest');
 		});
 		it('creates dependency for each injection', function() {
-			
+	        var object = {
+	        	number: 0,
+	        	setCounter: function(number) {
+	        		// Stores the provided number in an object
+					// attribute if this function is called.
+					this.number = number;
+	        	}	
+	        };
+	        injector.inject(object);
+	        assert.equal(object.number, 1);
+	        // On the second call the number should be incremented.
+	        injector.inject(object);
+	        assert.equal(object.number, 2);
 		});
 		it('throws exception if hook property is available, but not a function', function() {
-			
+			assert.throws(function() {
+				var object = {
+				    // This is named like a dependency hook, but
+				    // it is not a function.
+					setCounter: 42	
+				};
+				injector.inject(object);
+			});
 		});
 	});
 	
