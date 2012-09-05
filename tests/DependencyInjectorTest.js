f175ca38c7394be885d8e7a1edd570a85a0d6882
Matthias Molitor <matthias@matthimatiker.de>
2012-09-05 17:21:52 +0200

    added test setup

--- tests/DependencyInjectorTest.js
+++ tests/DependencyInjectorTest.js
@@ -1,7 +1,38 @@
 var assert = require("assert");
 
-var User = require("../ServerComponents/DependencyInjector.js");
+var DependencyInjector = require("../ServerComponents/DependencyInjector.js");
 
 describe('DependencyInjector', function() {
+	
+	/**
+	 * System under test.
+	 * 
+	 * @var {DependencyInjector.class}
+	 */
+	var injector = null;
+	
+	/**
+	 * Is executed before each test runs and sets up the environment.
+	 */
+	beforeEach(function() {
+		var runningNumber = 0;
+		var dependencies = {
+		    'setName': function() {
+		    	return 'GeoQuest';
+		    },
+			'setCounter': function() {
+				runningNumber++;
+				return runningNumber;
+			}
+		};
+		injector = new DependencyInjector.class(dependencies);
+	});
+	
+	/**
+	 * Removes instances that were created for testing after each test.
+	 */
+	afterEach(function() {
+		injector = null;
+	});
 	
 });
