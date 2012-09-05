3cc61cfc739e3b3e36d4532b7188f931545d61e5
RB <rebedeub@yahoo.com>
2012-09-05 14:51:47 +0200

    minor reformatting

--- ServerComponents/server.js
+++ ServerComponents/server.js
@@ -93,22 +93,22 @@ var express = require("express");
 var app = express();
 
 app.configure(function() {
-  app.set('port', process.env.PORT || 3000)
-  app.set('views', 'views')
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', 'views');
   app.engine('html', require('ejs').renderFile);
-  app.set("view options", {layout: false})
-  app.use(express.favicon())
-  app.use(express.logger('dev'))
-  app.use(express.cookieParser('scrt'))
-  app.use(express.session())
-  app.use(everyauth.middleware())
-  app.use(express.bodyParser())
-  app.use(express.methodOverride())
-  app.use(express.static(__dirname))
+  app.set("view options", {layout: false});
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.cookieParser('scrt'));
+  app.use(express.session());
+  app.use(everyauth.middleware());
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.static(__dirname));
   app.use(preEveryauthMiddlewareHack());
   app.use(everyauth.middleware());
   app.use(postEveryauthMiddlewareHack());
-})
+});
 
 
 
