5fef81f0683ee11302adf07f15c8cb75bc881cfd
Matthias Molitor <matthias@matthimatiker.de>
2012-09-05 11:26:47 +0200

    added helper functions and basic functionality

--- ServerComponents/UserDataAccess.js
+++ ServerComponents/UserDataAccess.js
@@ -9,6 +9,8 @@
  * </code>
  */
 
+var User = require('./User');
+
 UserRepository = function(connection) 
 {
 	if (connection === undefined) {
@@ -17,6 +19,13 @@ UserRepository = function(connection)
 	if(connection === null) {
 		throw new Error("Invalid connection or connection could not be established!");
 	}
+	
+	/**
+	 * The MongoDB database connection.
+	 * 
+	 * @var {Object}
+	 */
+	this.connection = connection;
 };
 
 /**
@@ -38,7 +47,8 @@ UserRepository = function(connection)
  */
 UserRepository.prototype.byGoogleIdentifier = function(identifier, callback)
 {
-	
+	var query = {'loginType': 'Google', 'identifier': identifier};
+	this.connection.users.find(query, this._createResultHandler(callback));
 };
 
 /**
@@ -62,6 +72,46 @@ UserRepository.prototype.byFacebookIdentifier = function(identifier, callback)
 UserRepository.prototype.byGeoQuestIdentifier = function(identifier, callback)
 {
 	
+};
+
+/**
+ * Creates a callback that handles a MongoDB result.
+ * 
+ * Throws an exception if an error occurs. In case of
+ * a successful result it will convert the result set 
+ * into a User object (or null if not found) and 
+ * pass it to the provided callback.
+ * 
+ * @param {function}
+ * @return {function}
+ * @throws Error If an internal error occurred.
+ */
+UserRepository.prototype._createResultHandler = function(callback) {
+	// Store the current context as the scope changes in the callback.
+	var self = this;
+	return function(error, result) {
+		if (error) {
+			throw new Error(error);
+		}
+		// Convert result to model.
+		callback(self._jsonToUser(result));
+	};
+};
+
+/**
+ * Receives a MongoDB result set and converts it into a User object.
+ * 
+ * Converts the result set to null if it is empty.
+ * 
+ * @param {Object} result A JSON object that contains the result set.
+ * @return {User.class}|null
+ */
+UserRepository.prototype._jsonToUser = function(result) {
+	if (result.count() === 0) {
+		// No user was found.
+		return null;
+	}
+	return new User.fromJSON(result[0]);
 };
 
 exports.class = UserRepository;
