31f37b6cd69a9042923b3940029b06d9f547f10d
Matthias Molitor <matthias@matthimatiker.de>
2012-09-06 10:11:49 +0200

    added configuration stub for routing/pages

---
+++ ServerComponents/conf/pages.js
@@ -0,0 +1,12 @@
+/**
+ * This configurations maps routes to pages that handle the requests.
+ * 
+ * The route is used as property name. The value is an object that specifies
+ * the page module that handles the incoming requests.
+ */
+var pages = {
+		
+		
+};
+
+exports = pages;
