5fc58962ff2943d23983dc57616a2d5dc5f8d579
Matthias Molitor <matthias@matthimatiker.de>
2012-09-06 11:11:55 +0200

    added test stubs for pages configuration

---
+++ tests/conf/pagesTest.js
@@ -0,0 +1,16 @@
+var assert = require("assert");
+
+var pages = require("../../ServerComponents/conf/pages");
+
+describe('Page configuration', function() {
+	it('returns an object', function() {
+		
+	});
+	it('contains at least one page', function() {
+		
+	});
+	it('contains required attributes per page', function() {
+		
+	});
+});
+	
