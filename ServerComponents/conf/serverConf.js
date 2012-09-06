75796207752619137a351fe6d92fbecfe737d64c
Matthias Molitor <matthias@matthimatiker.de>
2012-09-06 12:01:08 +0200

    added options to server configuration

--- ServerComponents/conf/serverConf.js
+++ ServerComponents/conf/serverConf.js
@@ -2,3 +2,15 @@
  * This file exports configuration options 
  * that modify the server behavior.
  */
+var serverConfiguration = {
+    // The port the server listens on.
+	'port': 3000,
+	// Defines the template engine that is used.
+	'view engine': 'ejs',
+	// The directory that contains the view templates.
+	'views': __dirname + '/../views',
+	// Directory that contains public contents.
+	'public': __dirname + '/../public'
+};
+
+module.exports = serverConfiguration;
