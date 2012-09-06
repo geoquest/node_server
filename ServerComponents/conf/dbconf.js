21f173a804c71d6df2c607d82f9c23cbc5a0c4a1
Matthias Molitor <matthias@matthimatiker.de>
2012-09-06 13:35:59 +0200

    registered user collection for mongo db

--- ServerComponents/conf/dbconf.js
+++ ServerComponents/conf/dbconf.js
@@ -13,7 +13,7 @@ module.exports = {
 
 var dbconf = {
 	url: host + ":" + port + "/" + dbname
-  , collections : ["REGISTERED"]
+  , collections : ["REGISTERED", "users"]
   , salt : '1@#4%^78()'
 };
 
