ea9698b92adaa8a5a87e527cc5ccaef19023c62a
unknown <guest-wels@POOL-C-00.bit.uni-bonn.de>
2012-09-07 12:34:10 +0200

    added everyauth skeleton

--- ServerComponents/application.js
+++ ServerComponents/application.js
@@ -6,28 +6,53 @@
  * node application.js
  * </code>
  */
-
 var crypto = require('crypto');
 var express = require("express");
+var serverConf = require('./conf/serverConf');
+//login dependencies
+var GeoQuest = require('./pages/login/GeoQuest');
+var everyauth = require('everyauth');
+var extAuthConf = require('./conf/extAuthConf');
+
+
 var app = express();
 
-var serverConf = require('./conf/serverConf');
+//configure everyauth
+everyauth
+	.facebook
+	.appId(extAuthConf.fb.appId)
+	.appSecret(extAuthConf.fb.appSecret)
+	.findOrCreateUser( function (session, accessToken, accessTokenExtra, fbUserMetadata) {
+		//implement login logic
+	})
+	.redirectPath('/');
+
+everyauth.google
+	.appId(extAuthConf.google.clientId)
+	.appSecret(extAuthConf.google.clientSecret)
+	.scope('https://www.googleapis.com/auth/userinfo.profile https://www.google.com/m8/feeds/')
+	.findOrCreateUser( function (sess, accessToken, extra, googleUser) {
+		//implement login logic
+	})
+	.redirectPath('/');
 
 app.configure(function() {
-  app.set('port', serverConf.port);
-  // Set the path to the view templates.
-  app.set('views', serverConf.views);
-  // Use the EJS engine for template rendering.
-  app.set('view engine', serverConf.viewEngine);
-  // Provides access to cookies as part of the request object.
-  app.use(express.cookieParser());
-  // Provides some basic session handling in memory.
-  app.use(express.session({'secret': crypto.randomBytes(64)}));
-  // Parses the body, supports form handling.
-  // http://www.senchalabs.org/connect/bodyParser.html
-  app.use(express.bodyParser());
-  // Exposes the contents in the given directory to the public.
-  app.use(express.static(serverConf.public));
+	app.set('port', serverConf.port);
+	// Set the path to the view templates.
+	app.set('views', serverConf.views);
+	// Use the EJS engine for template rendering.
+	app.set('view engine', serverConf.viewEngine);
+	// Provides access to cookies as part of the request object.
+	app.use(express.cookieParser());
+	// Provides some basic session handling in memory.
+	app.use(express.session({'secret': crypto.randomBytes(64)}));
+	// Parses the body, supports form handling.
+	// http://www.senchalabs.org/connect/bodyParser.html
+	app.use(express.bodyParser());
+	// Exposes the contents in the given directory to the public.
+	app.use(express.static(serverConf.public));
+	// Provides everyauth functionalities
+	app.use(everyauth.middleware());
 });
 
 var dependencies = {
@@ -66,4 +91,5 @@ for (var route in pages) {
 	app.all(route, handler);
 }
 
+//start server
 app.listen(app.get('port'));
