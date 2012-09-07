053bdd0afe47927242a4a5e5d12e783e29ec6791
Kuan-Lin Chiu <cklppt@gmail.com>
2012-09-07 15:37:06 +0200

    Add parameters pass to login.ejs.

--- ServerComponents/pages/Home.js
+++ ServerComponents/pages/Home.js
@@ -6,7 +6,7 @@ Home = function() {
 
 Home.prototype.handleRequest = function(request, response)
 {
-	response.render('login.ejs');
+	response.render('login.ejs', { title: 'Log In' , msg: 'login using your GeoQuest account'});
 };
 
 exports.class = Home;
