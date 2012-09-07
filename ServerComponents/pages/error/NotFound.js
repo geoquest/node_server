4e07e06bcf978718f29ae16231c4484c82a4bbfe
Matthias Molitor <matthias@matthimatiker.de>
2012-09-07 12:33:23 +0200

    implemented NotFound page module

--- ServerComponents/pages/error/NotFound.js
+++ ServerComponents/pages/error/NotFound.js
@@ -4,9 +4,16 @@ NotFound = function() {
 	
 };
 
+/**
+ * Renders a "Page not found" template and sends the appropriate HTTP code.
+ * 
+ * @param {Object} request
+ * @param {Object} response
+ */
 NotFound.prototype.handleRequest = function(request, response)
 {
-	
+	response.statusCode = 404;
+	response.render('error/not-found.ejs');
 };
 
 exports.class = NotFound;
