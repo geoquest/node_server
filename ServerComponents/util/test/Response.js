2383be7177141df0a9d78ed5a2e7e3b361df5de8
Matthias Molitor <matthias@matthimatiker.de>
2012-09-10 15:22:45 +0200

    added comments

--- ServerComponents/util/test/Response.js
+++ ServerComponents/util/test/Response.js
@@ -1,4 +1,6 @@
-
+/**
+ * Simulates a response object that can be used for testing.
+ */
 
 Response = function() 
 {
@@ -8,12 +10,29 @@ Response = function()
 	this.redirectStatus = null;
 };
 
+/**
+ * Simulates the rendering of a template.
+ * 
+ * @param {String} template Name of the template.
+ * @param {Object}|undefined templateVars Variables that are passed to the template (optional).
+ */
 Response.prototype.render = function(template, templateVars)
 {
 	this.template = template;
 	this.templateVars = templateVars || {};
 };
 
+/**
+ * Simulates a redirect.
+ * 
+ * The status code is optional and can be omitted:
+ * <code>
+ * response.redirect('/home');
+ * </code>
+ * 
+ * @param {integer}|{String} status
+ * @param {String}|undefined url
+ */
 Response.prototype.redirect = function(status, url) {
 	if(url === undefined){
 		url = status;
