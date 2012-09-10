d3a8823a908d6cb2fbabf260f208cb1146dd98ef
Matthias Molitor <matthias@matthimatiker.de>
2012-09-10 15:16:56 +0200

    implemented tests

--- tests/util/test/ResponseTest.js
+++ tests/util/test/ResponseTest.js
@@ -33,53 +33,60 @@ describe('Response', function() {
 	
 	describe('template attribute', function() {
 		it('is initially null', function() {
-			
+			assert.equal(response.template, null);
 		});
 	});
 	
 	describe('templateVars attribute', function() {
 		it('is initially null', function() {
-			
+			assert.equal(response.templateVars, null);
 		});
 	});
 	
 	describe('redirectUrl attribute', function() {
 		it('is initially null', function() {
-			
+			assert.equal(response.redirectUrl, null);
 		});
 	});
 	
 	describe('redirectStatus attribute', function() {
 		it('is initially null', function() {
-			
+			assert.equal(response.redirectStatus, null);
 		});
 	});
 	
 	describe('render', function() {
 		it('stores template name in template attribute', function() {
-			
+			response.render("name.ejs");
+			assert.equal(response.template, "name.ejs");
 		});
 		it('stores template variables in templateVars attribute', function() {
-			
+			response.render("name.ejs", {'title' : 'Hallo'});
+			assert.equal(response.templateVars['title'], 'Hallo');
 		});
 		it('stores empty object in templateVars attribute if variables are omitted', function() {
-			
+			response.render("name.ejs");
+			assert.deepEqual(response.templateVars, {});
 		});
 		
 	});
 	
 	describe('redirect function', function() {
 		it('stores status code in redirectStatus attribute', function() {
-			
+			response.redirect(302 , "haha.de");
+			assert.equal(response.redirectStatus, 302);
 		});
 		it('stores url in redirectUrl attribute', function() {
-			
+			response.redirect(302 , "haha.de");
+			assert.equal(response.redirectUrl, "haha.de");
 		});
 		it('stores correct url if status is omitted', function() {
-			
+			response.redirect("haha.de");
+			assert.equal(response.redirectUrl, "haha.de");
 		});
 		it('stores status code 302 if status is omitted', function() {
-			
+			response.redirect("haha.de");
+			assert.equal(response.redirectStatus, 302);
 		});
 	});
 
