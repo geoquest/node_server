34268b85dc4750fb540ab45719b3b920dde75b98
Matthias Molitor <matthias@matthimatiker.de>
2012-09-10 14:48:40 +0200

    implemented request

--- ServerComponents/util/test/Request.js
+++ ServerComponents/util/test/Request.js
@@ -1,26 +1,24 @@
 
-/*request = {
-			'method': 'GET',
-			'params': {},
-			'session': {},
-			// Simulate param() function.
-			'param': function(name) {
-				if (name in this.params) {
-					return this.params[name];
-				}
-				return undefined;
-			}
-		};*/
-
 Request = function() 
 {
-	this.params = null;
-	this.session = null;
+	this.params = {};
+	this.session = {};
 };
 
+/**
+ * Returns the parameter with the provided name.
+ * 
+ * Returns undefined if the parameter is not available.
+ * 
+ * @param {String} name
+ * @return {Object}|undefined
+ */
 Request.prototype.param = function(name) 
 {
-	
+	if (name in this.params) {
+		return this.params[name];
+	}
+	return undefined;
 };
 
 exports.class = Request;
