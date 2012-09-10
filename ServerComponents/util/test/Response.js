817e1c9d7755e0b675a7a4ea8e59e0c95237fba9
Matthias Molitor <matthias@matthimatiker.de>
2012-09-10 14:55:43 +0200

    added response stub

---
+++ ServerComponents/util/test/Response.js
@@ -0,0 +1,19 @@
+
+
+Response = function() 
+{
+	this.template = null;
+	this.templateVars = null;
+	this.redirectUrl = null;
+};
+
+Response.prototype.render = function(template)
+{
+	
+};
+
+Response.prototype.redirect = function(status, url) {
+	this.redirectUrl = url;
+};
+
+exports.class = Response;
