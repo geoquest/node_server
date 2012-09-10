c6f5e357f751692eee44fa419e7a7a72d9f4ea5c
Matthias Molitor <matthias@matthimatiker.de>
2012-09-10 15:20:06 +0200

    implemented Response

--- ServerComponents/util/test/Response.js
+++ ServerComponents/util/test/Response.js
@@ -8,13 +8,20 @@ Response = function()
 	this.redirectStatus = null;
 };
 
-Response.prototype.render = function(template)
+Response.prototype.render = function(template, templateVars)
 {
-	
+	this.template = template;
+	this.templateVars = templateVars || {};
 };
 
 Response.prototype.redirect = function(status, url) {
+	if(url === undefined){
+		url = status;
+		status = 302;
+	}
+	this.redirectStatus = status;
 	this.redirectUrl = url;
+	
 };
 
 exports.class = Response;
