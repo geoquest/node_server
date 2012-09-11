1bdf24661c813c90224f89f639b31d57f9b22f2f
Matthias Molitor <matthias@matthimatiker.de>
2012-09-11 13:43:51 +0200

    integrated dispatcher into application.js

--- ServerComponents/application.js
+++ ServerComponents/application.js
@@ -89,27 +89,13 @@ var dependencies = {
 };
 var DependencyInjector = require('./DependencyInjector');
 var injector = new DependencyInjector.class(dependencies);
-
+var PageDispatcher = require('./PageDispatcher');
+var dispatcher = new PageDispatcher.class(__dirname + '/pages', injector);
 
 var pages = require(__dirname + '/conf/pages');
 for (var route in pages) {
 	var pageInfo = pages[route];
-	// Use a closure to create the handler function.
-	// The closure guarantees, that pageInfo contains 
-	// the correct value when the handler function is 
-	// called.
-	// Without the closure pageInfo would always contain
-	// the value of the last iteration.
-	// See http://stackoverflow.com/questions/750486/javascript-closure-inside-loops-simple-practical-example
-	// for a simple example of the problem and a simple solution.
-	var handler = function(pageInfo) {
-		return function(request, response) {
-			var module = require(__dirname + '/pages/' + pageInfo.module);
-			var page = new module.class();
-			injector.inject(page);
-			page.handleRequest(request, response);
-		};
-	}(pageInfo);
+	var handler = dispatcher.createHandlerFor(pageInfo);
 	app.all(route, handler);
 }
 
