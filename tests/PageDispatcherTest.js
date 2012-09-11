90d540f2fa5e9f7da46306374163301659d91c6e
Matthias Molitor <matthias@matthimatiker.de>
2012-09-11 12:25:02 +0200

    implemented some tests

--- tests/PageDispatcherTest.js
+++ tests/PageDispatcherTest.js
@@ -1,6 +1,13 @@
 var assert = require("assert");
 
+var pageModulesPath = __dirname + '/../ServerComponents/util/test';
+
+var Page = require("../ServerComponents/util/test/Page");;
+var Request = require("../ServerComponents/util/test/Request");
+var Response = require("../ServerComponents/util/test/Response");
+var User = require("../ServerComponents/User");
 var PageDispatcher = require("../ServerComponents/PageDispatcher");
+
 
 describe('PageDispatcher', function() {
 
@@ -17,6 +24,27 @@ describe('PageDispatcher', function() {
 	 * @var {Object}
 	 */
 	var injector = null;
+	
+	/**
+	 * Example page configuration.
+	 * 
+	 * @var {Object}
+	 */
+	var config = null;
+	
+	/**
+	 * A simulated request object.
+	 * 
+	 * @var {Object}
+	 */
+	var request = null;
+	
+	/**
+	 * A simulated response object.
+	 * 
+	 * @var {Object}
+	 */
+	var response = null;
 
 	/**
 	 * Is executed before each test runs and sets up the environment.
@@ -28,7 +56,13 @@ describe('PageDispatcher', function() {
 				return object;
 			}
 		};
-		dispatcher = new PageDispatcher.class('path', injector);
+		// Default page configuration.
+		config = {
+			'module': 'Page'
+		};
+		request = new Request.class();
+		response = new Response.class();
+		dispatcher = new PageDispatcher.class(pageModulesPath, injector);
 	});
 
 	/**
@@ -36,36 +70,61 @@ describe('PageDispatcher', function() {
 	 */
 	afterEach(function() {
 		dispatcher = null;
+		response = null;
+		request = null;
+		config = null;
 		injector = null;
 	});
 
 	describe('constructor', function() {
 		it('throws exception if page module path is omitted', function() {
-
+			assert.throws(function() {
+				new PageDispatcher.class(undefined, injector);
+			});
 		});
 		it('throws exception if module path does not exist', function() {
-
+			assert.throws(function() {
+				new PageDispatcher.class(__dirname + '/missing/directory', injector);
+			});
 		});
 		it('throws exception if injector is omitted', function() {
-
+			assert.throws(function() {
+				new PageDispatcher.class(pageModulesPath);
+			});
 		});
 	});
 	
 	describe('createHandlerFor', function() {
 		it('returns function', function() {
-			
+			var handler = dispatcher.createHandlerFor(config);
+			assert.equal(typeof handler, 'function');
 		});
 		it('loads required module', function() {
-			
+			var handler = dispatcher.createHandlerFor(config);
+			var page = handler(request, response);
+			assert.ok(page instanceof Page.class);
 		});
-		it('passes module to dependency injector', function() {
-			
+		it('passes module to dependency injector', function(done) {
+			injector.inject = function(object) {
+				assert.ok(object instanceof Page.class);
+				done();
+				return object;
+			};
+			var handler = dispatcher.createHandlerFor(config);
+			handler(request, response);
 		});
 		it('passes request and response to handleRequest()', function() {
-			
+			var handler = dispatcher.createHandlerFor(config);
+			var page = handler(request, response);
+			assert.ok(page instanceof Page.class);
+			assert.strictEqual(page.request, request);
+			assert.strictEqual(page.response, response);
 		});
 		it('calls handleRequest() if no restrictions are configured', function() {
-			
+			var handler = dispatcher.createHandlerFor(config);
+			var page = handler(request, response);
+			assert.ok(page instanceof Page.class);
+			assert.strictEqual(page.called, true);
 		});
 		it('calls handleRequest() if page is restricted to user and user is logged in', function() {
 			
