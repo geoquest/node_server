5770e6d6a887be7c46ada470fbc238b9c37a054e
Matthias Molitor <matthias@matthimatiker.de>
2012-09-12 10:37:10 +0200

    added stub for middleware that exposes some variables to the views

---
+++ ServerComponents/middleware/TemplateVariables.js
@@ -0,0 +1,13 @@
+
+/**
+ * @param {Object} request
+ * @param {Object} response
+ * @param {function} next
+ */
+var importVariables = function(request, response, next) {
+	
+};
+
+module.exports.importVariables = function() {
+	return importVariables;
+};
