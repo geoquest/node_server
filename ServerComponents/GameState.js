455c8a12143f9c539e4b644e890acefa61f2cdf1
Matthias Molitor <matthias@matthimatiker.de>
2012-09-12 16:34:19 +0200

    implemented gameState model

--- ServerComponents/GameState.js
+++ ServerComponents/GameState.js
@@ -1,5 +1,11 @@
 GameState = function(json){
-	
+	if(!((typeof json)=='object')){
+		throw(new Error("object not received"))
+	}
+	this.state = json;	
 }
-GameState.prototype.getGameSessionId = function(){};
+GameState.prototype.getGameSessionId = function(){
+	var sessionId = this.state.id;
+	return sessionId;
+};
 exports.class = GameState;
