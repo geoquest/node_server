05a75a2d7812c796cc1575e7eae1811c809908ee
Matthias Molitor <matthias@matthimatiker.de>
2012-09-12 10:50:14 +0200

    implemented middleware that exposes the currently logged in user to the view templates

--- ServerComponents/middleware/TemplateVariables.js
+++ ServerComponents/middleware/TemplateVariables.js
@@ -1,11 +1,29 @@
 
 /**
+ * Passes variables to the view templates.
+ * 
  * @param {Object} request
  * @param {Object} response
  * @param {function} next
  */
 var importVariables = function(request, response, next) {
-	
+	request.app.locals.user = _getUser(request.session);
+	next();
+};
+
+/**
+ * Extracts the user data from the session.
+ * 
+ * Returns null if the user is not logged in.
+ * 
+ * @param {Object} session
+ * @returns {Object}|null
+ */
+function _getUser(session) {
+	if (session.user) {
+		return session.user;
+	}
+	return null;
 };
 
 module.exports.importVariables = function() {
