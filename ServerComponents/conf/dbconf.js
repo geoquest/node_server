fce238ab3dd255a4ac9ad28e0afc47f11cfcbfbc
Matthias Molitor <matthias@matthimatiker.de>
2012-09-13 15:29:57 +0200

    removed salt as it is not used anymore the salts are managed by User class now

--- ServerComponents/conf/dbconf.js
+++ ServerComponents/conf/dbconf.js
@@ -14,7 +14,6 @@ module.exports = {
 var dbconf = {
 	url: host + ":" + port + "/" + dbname
   , collections : ["users", "games", "gameStates"]
-  , salt : '1@#4%^78()'
 };
 
 module.exports = dbconf;
