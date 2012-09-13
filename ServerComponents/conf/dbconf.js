ea3c16183a25708ee16ba2a57de9f34b8793192e
Matthias Molitor <matthias@matthimatiker.de>
2012-09-13 15:30:23 +0200

    removed code that was commented out

--- ServerComponents/conf/dbconf.js
+++ ServerComponents/conf/dbconf.js
@@ -2,15 +2,6 @@ var dbname = "test";
 var host = "localhost";
 var port = "27017";
 
-/*
-module.exports = {
-	url: host + ":" + port + "/" + dbname
-  , collections : ["REGISTERED"]
-  , salt : '1@#4%^78()'
-};
-*/
-
-
 var dbconf = {
 	url: host + ":" + port + "/" + dbname
   , collections : ["users", "games", "gameStates"]
