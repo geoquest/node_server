f594dab2d524caa98080a69c08f500931dad8841
Pascal Bihler <bihler@cs.uni-bonn.de>
2012-09-14 11:05:35 +0200

    next try fix restart script

--- scripts/restart_server.sh
+++ scripts/restart_server.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-BUILD_ID=
 [ -e /tmp/gqns.pid ]  && kill `cat /tmp/gqns.pid` || true
 rm -rf /var/lib/jenkins/live/geoquest_node_server
 mv /var/lib/jenkins/live/geoquest_node_server.pre /var/lib/jenkins/live/geoquest_node_server
 cd /var/lib/jenkins/live/geoquest_node_server/ServerComponents
+BUILD_ID=
 nohup node application.js & echo $! > /tmp/gqns.pid
