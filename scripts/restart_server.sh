48e025812cfbce8c18f06105d10c87940f0d7519
Pascal Bihler <bihler@cs.uni-bonn.de>
2012-09-14 12:32:43 +0200

    Cleaning live dir before move

--- scripts/restart_server.sh
+++ scripts/restart_server.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 [ -e /tmp/gqns.pid ] && [ -f /proc/`cat /tmp/gqns.pid`/exe ] && kill `cat /tmp/gqns.pid`
+rm -rf /var/lib/jenkins/live/geoquest_node_server
 mv /var/lib/jenkins/live/geoquest_node_server.pre /var/lib/jenkins/live/geoquest_node_server
 cd /var/lib/jenkins/live/geoquest_node_server/ServerComponents
 BUILD_ID=
