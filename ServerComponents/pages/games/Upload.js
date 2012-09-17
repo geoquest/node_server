d4e071034f7ab7b0f66190040016798f9f8732b9
Matthias Molitor <matthias@matthimatiker.de>
2012-09-17 11:36:56 +0200

    refactoring: extracted methods

--- ServerComponents/pages/games/Upload.js
+++ ServerComponents/pages/games/Upload.js
@@ -27,46 +27,51 @@ Upload.prototype.setGameValidator = function(validator) {
 Upload.prototype.handleRequest = function(request, response) {
 	
 	if (request.method === 'GET') {
-		this.renderUploadForm(response, 'Please upload your game in JSON format.');
+		this._handleGet(request, response);
+	} else if (request.method === 'POST') {
+		this._handlePost(request, response);
+	}
+};
+
+Upload.prototype._handleGet = function(request, response) {
+	this.renderUploadForm(response, 'Please upload your game in JSON format.');
+};
+
+Upload.prototype._handlePost = function(request, response) {
+	// Check if a file has been provided
+	if (!request.files || !request.files.game || !request.files.game.path || !request.files.game.name) {
+		this.renderUploadForm(response, 'Error! Please choose a file to upload.');
+		return;
 	}
 	
-	if (request.method === 'POST') {
-
-		// Check if a file has been provided
-		if (!request.files || !request.files.game || !request.files.game.path || !request.files.game.name) {
-			this.renderUploadForm(response, 'Error! Please choose a file to upload.');
+	// Read file contents and try to parse it as JSON
+	var content = fs.readFileSync(request.files.game.path, 'utf8');
+	try {			
+		content = JSON.parse(content);
+		var valid = this._gameValidator.validateGame(content);
+		if (valid == false){
+			this.renderUploadForm(response, 'Error! Not a proper game file.');
 			return;
 		}
 		
-		// Read file contents and try to parse it as JSON
-		var content = fs.readFileSync(request.files.game.path, 'utf8');
-		try {			
-			content = JSON.parse(content);
-			var valid = this._gameValidator.validateGame(content);
-			if (valid == false){
-				this.renderUploadForm(response, 'Error! Not a proper game file.');
-				return;
-			}
-			
-		} catch(err) {
-			this.renderUploadForm(response, 'Error! Not a legal JSON file.');
-			return;
-		}
-
-		// Upload successful
-		var params = { title: 'Game Upload Response', msg: 'Your game has been uploaded successfully.'};
-		
-		var gameData = {
-			'authors': [request.session.user.getId()],
-			'content': content
-		};
-        var game = new Game.fromJSON(gameData);      
-	 
-		
-		this._gameRepository.insert(game);
-
-		response.render('upload-response', params);
+	} catch(err) {
+		this.renderUploadForm(response, 'Error! Not a legal JSON file.');
+		return;
 	}
+
+	// Upload successful
+	var params = { title: 'Game Upload Response', msg: 'Your game has been uploaded successfully.'};
+	
+	var gameData = {
+		'authors': [request.session.user.getId()],
+		'content': content
+	};
+    var game = new Game.fromJSON(gameData);      
+ 
+	
+	this._gameRepository.insert(game);
+
+	response.render('upload-response', params);
 };
 
 Upload.prototype.renderUploadForm = function(response, msgParam) {
