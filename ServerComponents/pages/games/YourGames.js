93f6e478f87baeb0af12ba89b81299034eedcf3e
Matthias Molitor <matthias@matthimatiker.de>
2012-09-17 14:21:25 +0200

    partially integrated both pages

--- ServerComponents/pages/games/YourGames.js
+++ ServerComponents/pages/games/YourGames.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 YourGames = function() {
 	this._gameRepository = null;
 	this._gameValidator = null;
-	this._template = null;
+	this._template = 'games/your-games.ejs';
 	this._templateVariables = {};
 };
 
@@ -27,7 +27,63 @@ YourGames.prototype.setGameValidator = function(validator) {
 };
 
 YourGames.prototype.handleRequest = function(request, response) {
+	var self = this;
+	var user = request.session.user;
+	this._gameRepository.findAllByUser(user, function(games) {
+		self._templateVariables.games = games;
+		self._setMessage('Please upload your game in JSON format.');
+		if (request.method === 'POST') {
+			self._handleUpload(request);
+		}
+		response.render(self._template, self._templateVariables);
+	});
 	
+};
+
+/**
+ * Handles POST requests to upload games.
+ * 
+ * @param {Object} request
+ */
+YourGames.prototype._handleUpload = function(request) {
+	// Check if a file has been provided
+	if (!request.files || !request.files.game || !request.files.game.path || !request.files.game.name) {
+		this._setMessage('Error! Please choose a file to upload.');
+		return;
+	}
+	
+	// Read file contents and try to parse it as JSON
+	var content = fs.readFileSync(request.files.game.path, 'utf8');
+	try {			
+		content = JSON.parse(content);
+		var valid = this._gameValidator.validateGame(content);
+		if (valid == false){
+			this._setMessage('Error! Not a proper game file.');
+			return;
+		}
+		
+	} catch(err) {
+		this._setMessage('Error! Not a legal JSON file.');
+		return;
+	}
+	
+	// Upload successful
+	var gameData = {
+		'authors': [request.session.user.getId()],
+		'content': content
+	};
+    var game = new Game.fromJSON(gameData);      
+ 
+	this._gameRepository.insert(game);
+	
+	// todo: necessary?
+	this._template = 'upload-response';
+	this._templateVariables.title = 'Game Upload Response';
+	this._setMessage('Your game has been uploaded successfully.');
+};
+
+YourGames.prototype._setMessage = function(message) {
+	this._templateVariables.msg = message;
 };
 
 exports.class = YourGames;
