0a0344784d49abdb61d2f9d6875af163dd2ce51c
Vadim Richter <richter@cs.uni-bonn.de>
2012-09-18 12:35:00 +0200

    Final version of the Mobile Login Module

--- ServerComponents/pages/login/GeoQuestMobile.js
+++ ServerComponents/pages/login/GeoQuestMobile.js
@@ -27,17 +27,12 @@ GeoQuestMobileLogin.prototype.setUserRepository = function(repository)
  * @param {Object} response
  */
 GeoQuestMobileLogin.prototype.handleRequest = function(request, response) {
-	console.log("got to the handleRequest " + request.method);
 	if (request.method == 'POST') {
 		var data = request.body;		 
 		var username = data['username'];
         var rawPassword = data['password'];
-        
-        console.log( );
-        
         this._userRepository.byGeoQuestIdentifier(username, function(userOrNull) {
         	if (userOrNull === null) {
-        		//console.log(username + "   " +rawPassword+"   user not found");
         		responseText = {'success':false,  'msg': 'User not found.'};
         		response.setHeader("Content-Type", "application/json;charset=UTF-8");
         		response.write(JSON.stringify(responseText));
@@ -46,17 +41,17 @@ GeoQuestMobileLogin.prototype.handleRequest = function(request, response) {
         	}
         	var user = userOrNull;
         	if (user.hasPassword(rawPassword)) {
-        		console.log(username + "   " +rawPassword+"   LOGIN Attempt Successful!!!!");
-        		// Credentials are correct, therefore store the user in the session.  
-        		responseText = {'success':true,  'msg': 'Welcome ' + user + '!', fullUser: user.toJSON()};
+        		// Credentials are correct, therefore store the user in the session.
+        		var givenUser = user.toJSON();
+        		delete givenUser.password;
+        		delete givenUser._id;
+        		responseText = {'success':true,  'msg': 'Welcome ' + user + '!', fullUser: givenUser};
         		response.setHeader("Content-Type", "application/json;charset=UTF-8");
         		response.write(JSON.stringify(responseText));
         		request.session.user = user;
-        		console.log(request.session.user);
         		response.end();
         	} else {
         		// Wrong password provided.
-        		//console.log(username + "   " +rawPassword  + "   wrong password");
         		responseText = {'success':false,  'msg': 'Wrong password. Please retry.'};
         		response.setHeader("Content-Type", "application/json;charset=UTF-8");
         		response.write(JSON.stringify(responseText));
