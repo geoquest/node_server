3cbc9fe0ba59231c9d006f5b2fe77e82572626de
Vadim <vadimcostache@gmail.com>
2012-09-19 17:10:13 +0200

    added : loadGamesList loadResourcesList getResource

--- ServerComponents/GridFSConnection.js
+++ ServerComponents/GridFSConnection.js
@@ -84,35 +84,82 @@ GridFSConnection.prototype.saveFile = function(newFilename, fileToWrite,
 	} else {
 		this._eventBuffer.push(event);
 		this._openConnection();
-	}
-	;
+	};
 };
 
-GridFSConnection.prototype.loadGamesList = function(userId){
+GridFSConnection.prototype.loadGamesList = function(userId, callback){
 	
 	
 	var gamesList = [];
 	
-	db.open(function(err, db) {		
-		
-		
+	db.open(function(err, db) {
 		
 		//getting the games list for the given author (search is done by ID)
 		db.collection('games', function(err, collection) {
 			collection.find({'authors': userId}, function(err, cursor){
-		        cursor.toArray(function(err, docs){
-		        	
+		        cursor.toArray(function(err, docs){		        	
 		        	for (i in items){
 		        		gamesList.add(docs[i]._id);	        		
 		        	}
+		        	
+		        	callback(gamesList);
 		        });
 			});
 		});
-	
-		
 	});
 };
+
+
+GridFSConnection.prototype.loadResourcesList = function(gameId, callback){
 	
+	var resourcesIDs = [];	
+	var id = ObjectID.createFromHexString(gameId);
+	
+	db.open(function(err, db) {
+		
+		//getting the resources list for the given author (search is done by ID)
+		db.collection('fs.files', function(err, collection) {
+			collection.find({"metadata.game_id" : id}, function(err, cursor){				
+		        cursor.toArray(function(err, docs){		        	
+		        	for (i in items){
+		        		gamesList.add(docs[i]._id);	        		
+		        	}		        	
+		        	callback(db, resourceIDs);
+		        });
+			});
+		});
+	});	
+};
+
+
+GridFSConnection.prototype.getResource = function(db, resourceID){
+	
+	db.open(function(err, db) {
+	
+		var id = ObjectID.createFromHexString(resourecID);	
+		var gridStore = new GridStore(db, id,"r");
+		var fileName = gridStore.filename;
+		
+		gridStore.open(function(err,gridStore){
+					
+			var fileStream = fs.createWriteStream("./"+fileName);
+			var stream = gridStore.stream(true);
+	
+			stream.on("data", function(chunk) {
+				fileStream.write(chunk);
+	        });
+			
+			
+			stream.on("end", function(err) {							
+				console.log(err);				
+				db.close();
+			});
+		});
+	});
+};
+
+
+
 		
 		
 		
